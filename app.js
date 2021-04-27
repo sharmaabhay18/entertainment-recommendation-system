26f7c7f53184da241587a268386592dda63b1ec2
Pratik Navasare <pratik.navasare@gmail.com>
2021-04-26 21:15:11 -0400

    Create landing and partials for header and footer

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.engine(
-  'handlebars',
+  '.hbs',
   exphbs({
     defaultLayout: 'main',
     helpers: helper,
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ const usersRoutes = require('./users');
 const routeConstructor = (app) => {
   app.use('/users', usersRoutes);
 
+  app.get('/', (req, res) => {
+    res.render('ERS/landing', {
+      title: 'Nav',
+    });
+  });
+
   // for testing if server is alive
   app.use('/ping', (_, res) => res.send('pong'));
   app.use('*', (_, res) => res.status(404).json('Page not found!'));
