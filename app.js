bd8cc9f406c56104babfc7283cec5f539012f3f8
Mihir Palyekar <mihirpalyekar@IGPTechs-MacBook-Air.local>
2021-04-27 21:36:31 +0530

    wip

--- app.js
+++ app.js
@@ -15,18 +15,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.engine(
-  '.hbs',
+  'handlebars',
   exphbs({
     defaultLayout: 'main', 
     helpers: helper,
   })
 );
 
-app.set('view engine', 'hbs');
+app.set('view engine', 'handlebars');
 app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
-
 app.use(express.static(publicDirectoryPath));
+
 
 app.use(
   session({
