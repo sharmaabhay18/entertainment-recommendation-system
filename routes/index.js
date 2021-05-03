9385dde8dd3aef4c18fdcfaa8651576f76868537
Abhay Sharma <asharm41@stevens.edu>
2021-05-03 16:31:18 -0400

    added comment rating functionality

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const usersRoutes = require('./users');
 const moviesRoutes = require('./movies');
 const ratingRoutes = require('./rating');
 const commentRoutes = require('./comments');
+const commentRatingRoutes = require('./commentRating');
 
 const routeConstructor = (app) => {
   app.use('/users', usersRoutes);
   app.use('/movies', moviesRoutes);
   app.use('/rating', ratingRoutes);
   app.use('/comment', commentRoutes);
+  app.use('/commentRating', commentRatingRoutes);
 
   // for testing if server is alive
   app.use('/ping', (_, res) => res.send('pong'));
