1da94ecac5d2b5fc469cde4120dd0820e4e5f020
Pratik Navasare <pratik.navasare@gmail.com>
2021-05-03 15:45:00 -0400

    User ListPage and add movie functionallity

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 const usersRoutes = require('./users');
+const addList = require('./addList')
 
 const routeConstructor = (app) => {
   app.use('/users', usersRoutes);
+  app.use('/addList',addList);
 
   app.get('/', (req, res) => {
-    res.render('ERS/landing', {
-      title: 'Nav',
-    });
+    res.render('ERS/landing');
   });
+
+  app.get('/addPopPage', async (req, res) => {
+    res.render('ERS/addList/addPopPage');
+  }); 
 
   // for testing if server is alive
   app.use('/ping', (_, res) => res.send('pong'));
