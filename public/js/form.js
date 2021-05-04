3d9b16b8fdb31868ddcc223cee839f5128745aeb
Pratik Navasare <pratik.navasare@gmail.com>
2021-05-04 18:34:52 -0400

    Created Movie list and Movie status updation forms

--- public/js/form.js
+++ public/js/form.js
@@ -13,11 +13,15 @@ const divElem = document.getElementById('show');
         errorDiv.hidden = true;
         $('#showList').empty();
         $('#show').empty();
+        $('#show').hide();
         let pageLaod = {
           method: 'GET',
           url: ' http://api.tvmaze.com/search/shows?q=' + textInput.value,
         };
         $.ajax(pageLaod).then(function (responseMessage) {
+          const html = `<div id="addHeader" style="display: none;">Movies</div>`;
+          $("#showList").append(html);
+          $("#addHeader").show();
           responseMessage.map((e) => {
             $('#showList').show();
             let li = document.createElement('li');
@@ -42,8 +46,54 @@ const divElem = document.getElementById('show');
   $(document).ready(function () {
     $(document).on('click', 'ul li a', function (event) {
       event.preventDefault();
+            $("#showList").hide();
+            $("#show").empty();
+            $("#show").show();
       let urll = $(this).attr('id');
-      console.log(urll);
+      let pageLaod = {
+        method: 'GET',
+        url: 'http://api.tvmaze.com/search/shows?q=' + urll,
+      };
+      $.ajax(pageLaod).then(function (responseMessage) {
+         $("#addHeader2").show();
+            let html = 
+             `<form method="POST" id="movieForm">` +
+            `<h1>${urll}</h1>` +
+            `<hr>`+
+            `<label for="Status">Status<span>*</span> : </label>`+
+            `<select name="status" id="Status" class ="inputtext" required>
+            <option value="0">Select status</option>
+            <option value="1">Currently Watching</option>
+            <option value="2">Completed</option>
+            <option value="3">On-Hold</option>
+            </select>` +
+            `<br/>`+
+            `<label for="score">Score<span>*</span> : </label>`+
+            `<select name="score" id="score" class ="inputtext" required>
+            <option value="0">Select score</option>
+            <option value="10">(5) Masterpiece</option>
+            <option value="9">(4) Great</option>
+            <option value="8">(3) Very Good</option>
+            <option value="7">(2) Good</option>
+            <option value="6">(1) Fine</option>
+            </select>`+
+            `<br/>`+
+            `<button type="submit" id="addButton1" >Add</button>`+
+            `<button type="submit" id="addButton2">Cancel</button>`+
+            `</form>`;
+            $("#show").append(html);
+        });
+      });
     });
-  });
+
+
+    $(document).ready(function () {
+        $(document).on('click', '#addButton2', function (event) {
+            event.preventDefault();
+            $("#addHeader2").hide();
+            $("#show").hide();
+            $("#showList").show(); 
+         });
+    });
+   
 })(window.jQuery);
