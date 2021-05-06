f4c98b3a32a75d3a454732bc891f45a9478e3796
Pratik Navasare <pratik.navasare@gmail.com>
2021-05-06 01:22:56 -0400

    Changes in editUser js file for status functionality

--- public/js/editUserList.js
+++ public/js/editUserList.js
@@ -58,43 +58,17 @@ const getUrlParameter = function getUrlParameter(sParam) {
     });
   }
 
-  // $(document).ready(function () {
-  //     $(document).on('click', '#editB', function (event) {
-  //       event.preventDefault();
-  //       let newStatus = $('#status').val();
-  //       let newRating = $('#rating').val();
-  //       if (newStatus != 0 && newRating != 0) {
-  //         const requestConfig = {
-  //           method: 'POST',
-  //           url: '/movies/add-movie',
-  //           contentType: 'application/json',
-  //           data: JSON.stringify({
-  //             status: newStatus,
-  //             externalId: newExternalId,
-  //             rating: newRating,
-  //           }),
-  //         };
-  //         $.ajax(requestConfig)
-  //           .then(function (responseMessage) {
-  //             window.location.href = '/addList';
-  //           })
-  //           .catch((error) => {
-  //             if (
-  //               error?.responseJSON?.message &&
-  //               typeof error?.responseJSON?.message === 'string' &&
-  //               error?.responseJSON?.message.length !== 0
-  //             ) {
-  //               alert(error.responseJSON.message);
-  //             } else {
-  //               alert('Something went wrong');
-  //             }
-  //             window.location.href = '/addList/addMovie';
-  //           });
-  //       } else {
-  //         event.preventDefault();
-  //         alert('Please provide all required fields');
-  //         $('#addHeader2').show();
-  //         $('#show').show();
-  //       }
-  //     });
+  $(document).ready(function() {
+    $(document).on('click', '#allMovies', function (event) {
+     event.preventDefault();
+     const data = $(this).data("value").serializeArray();
+     const k = JSON.parse(JSON.stringify(data))
+ 
+    console.log(k);
+    $.map(data, function(i) {
+        console.log(i)
+      });
+    });   
+   });
+
 })(window.jQuery);
